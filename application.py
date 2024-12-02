2634509a7d3f46439aba0896d1d1fd4967ef3bd4
LifanZeng <73094918+LifanZeng@users.noreply.github.com>
2024-12-01 19:26:22 -0800

    Update application.py

--- application.py
+++ application.py
@@ -2,8 +2,11 @@ from flask import Flask, render_template, request
 from transformers import pipeline
 from transformers import GPT2LMHeadModel, GPT2Tokenizer
 
-tokenizer = GPT2Tokenizer.from_pretrained('Lifan-Z/protGPT2_AMR_proteins-homolog-512-each')
-protgpt2 = pipeline('text-generation', model= 'Lifan-Z/protGPT2_AMR_proteins-homolog-512-each')
+# tokenizer = GPT2Tokenizer.from_pretrained('Lifan-Z/protGPT2_AMR_proteins-homolog-512-each')
+# protgpt2 = pipeline('text-generation', model= 'Lifan-Z/protGPT2_AMR_proteins-homolog-512-each')
+
+tokenizer = GPT2Tokenizer.from_pretrained('Lifan-Z/protGPT2_5')
+protgpt2 = pipeline('text-generation', model= 'Lifan-Z/protGPT2_5')
 
 application = Flask(__name__)
 @application.route('/', methods = ['GET'])
@@ -25,7 +28,6 @@ def seq_with_beginning():
 
     return render_template('index_with_output.html', my_output=sequences[0])
     # return render_template('index_with_output.html', my_output = sequences[0]['generated_text'][13:])
-    # return render_template('index_with_output.html', my_output = sequences[0])
 
 if __name__ == '__main__':
     application.run(host= "0.0.0.0", port=5000)
